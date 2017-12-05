176c35d62f3c004779b731fd3b284c01f5505ac3
leah.fuentes <leah.fuentes@site.edu.au>
2017-12-05 21:33:20 +0800

    add views

---
+++ application/helpers/utility_helper.php
@@ -0,0 +1,14 @@
+<?php
+
+    function assets_url(){
+         return base_url().'assets/';
+    }
+    /**
+     * Material Design Lite
+     * https://getmdl.io/
+     */
+    function mdl(){
+        return base_url().'assets/mdl/';
+    }
+
+?>
---
+++ application/views/head.php
@@ -0,0 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $page_title; ?></title>
+     <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
+    <link rel = "stylesheet" type = "text/css" href ="<?php echo mdl().'material.min.css'; ?>">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <script src="<?php echo mdl().'material.min.js'; ?>"> </script>
+    
+</head>
+
